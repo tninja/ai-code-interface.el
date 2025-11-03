7a09bb6a3136396c972f192ceb83883d17158980
tninja <tninja@gmail.com>
2025-11-02 19:18:03 -0800

    bump version

--- ai-code-interface.el
+++ ai-code-interface.el
@@ -1,7 +1,7 @@
 ;;; ai-code-interface.el --- AI code interface for editing AI prompt files -*- lexical-binding: t; -*-
 
 ;; Author: Kang Tu <tninja@gmail.com>
-;; Version: 0.49
+;; Version: 0.50
 ;; Package-Requires: ((emacs "26.1") (transient "0.8.0") (magit "2.1.0"))
 
 ;; SPDX-License-Identifier: Apache-2.0
