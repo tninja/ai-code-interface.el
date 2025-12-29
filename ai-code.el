d7c29d73db5904804ede5567e16165c2005684c7
tninja <tninja@gmail.com>
2025-12-28 20:56:03 -0800

    bump version, update history

--- ai-code.el
+++ ai-code.el
@@ -1,7 +1,7 @@
 ;;; ai-code.el --- AI code interface for editing AI prompt files -*- lexical-binding: t; -*-
 
 ;; Author: Kang Tu <tninja@gmail.com>
-;; Version: 0.72
+;; Version: 0.73
 ;; Package-Requires: ((emacs "26.1") (transient "0.8.0") (magit "2.1.0"))
 ;; URL: https://github.com/tninja/ai-code-interface.el
 
