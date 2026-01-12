f198c3da3dd7a3c3eb1a0186a121d0dc4f3068fa
tninja <tninja@gmail.com>
2026-01-12 06:49:51 -0800

    simplify

--- ai-code.el
+++ ai-code.el
@@ -1,4 +1,4 @@
-;;; ai-code.el --- Unified interface for AI coding CLI tool such as Codex, Copilot CLI, Opencode, Grok CLI, etc -*- lexical-binding: t; -*-
+;;; ai-code.el --- Unified interface for AI coding CLI such as Codex, Copilot CLI, Opencode, Grok CLI, etc -*- lexical-binding: t; -*-
 
 ;; Author: Kang Tu <tninja@gmail.com>
 ;; Version: 0.90
